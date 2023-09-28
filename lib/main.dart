304109a0809b9c85a05b2c7750dabb9d4ac8b130
HMZKA <hmzakh65@gmail.com>
2023-09-28 18:31:24 +0300

    column

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,27 +17,64 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.white,
             foregroundColor: Colors.blue,
             elevation: 10,
-            leading: Icon(Icons.menu),
-            title: Text("My Application"),
-            actions: [Icon(Icons.call), Icon(Icons.menu_book)],
+            leading: const Icon(Icons.menu),
+            title: const Text("My Application"),
+            actions: [const Icon(Icons.call), const Icon(Icons.menu_book)],
           ),
-          body: Align(
-            alignment: Alignment.center,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(100)),
-              width: 300,
-              height: 200,
-              child: const Center(
-                child: Text(
-                  text,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.amber,
-                      fontWeight: FontWeight.w600),
+          body: Container(
+            width: double.infinity,
+            color: Colors.amber,
+            child: Column(
+              //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+//              mainAxisSize: MainAxisSize.min,
+              // 1 +1 +1 +1=4
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    color: Colors.blue,
+                    child: const Text(
+                      "First Text",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
+                  ),
                 ),
-              ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.green,
+                    child: const Text(
+                      "Second Text",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.pink,
+                    child: const Text(
+                      "Third Text",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    color: Colors.purple,
+                    child: const Text(
+                      "Fourth Text",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                )
+              ],
             ),
           )),
     );
