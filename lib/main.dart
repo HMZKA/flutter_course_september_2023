4e5731b2820a145862d95979244fa2862a7d0b2a
HMZKA <hmzakh65@gmail.com>
2023-09-28 17:51:10 +0300

    container

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String text = "Welcome to our application";
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.blue,
-          elevation: 20,
-          leading: Icon(Icons.menu),
-          title: Text("My Application"),
-          actions: [Icon(Icons.call), Icon(Icons.menu_book)],
-        ),
-        body: Center(child: Text("Welcome to our application")),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.blue,
+            elevation: 10,
+            leading: Icon(Icons.menu),
+            title: Text("My Application"),
+            actions: [Icon(Icons.call), Icon(Icons.menu_book)],
+          ),
+          body: Align(
+            alignment: Alignment.center,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red, borderRadius: BorderRadius.circular(100)),
+              width: 300,
+              height: 200,
+              child: const Center(
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
